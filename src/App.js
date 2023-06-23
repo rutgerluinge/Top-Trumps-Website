abe4f8a1b2126ca480fc4305d444ba8c06e3be0a
Rutger <rutger@luinge.eu>
2023-06-23 15:52:38 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Body } from "./components/Body";
+import { Footer } from "./components/Footer";
+import { Navigator } from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./styling/teststyling.css";
+import "./styling/essentials.css";
+import { Home } from "./pages/home";
+import { Game } from "./pages/game";
+import { Play } from "./pages/play";
+import { Implementation } from "./pages/implementation";
+import { Results } from "./pages/results";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navigator></Navigator>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/implementation" element={<Implementation />} />
+        <Route path="/play" element={<Play />} />
+        <Route path="/results" element={<Results />} />
+
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
